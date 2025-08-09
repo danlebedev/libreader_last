4f1bb9d9351f5bdf0a43344a0705f2a8cc1c912f
danlebedev <lebedev.py@gmail.com>
2025-08-09 23:24:00 +0300

    complete index

--- reader_app/views.py
+++ reader_app/views.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render
+from xml.etree import ElementTree
+from xml.etree.ElementTree import Element
+import os
+
+
+LIBRARY_ROOT = '/home/user/Desktop/library_last'
+
 
 def index(request):
-    return render(request, 'reader_app/index.html')
+    structure  = load_structure(LIBRARY_ROOT)
+    context = {"data": structure}
+    return render(request, 'reader_app/index.html', context)
+
+
+def xml_to_json(element: Element):
+    result = {'name': element.text.strip(), 'list': []}
+    for node in element:
+        if len(node):
+            result['list'].append(xml_to_json(node))
+        else:
+            result['list'].append({'name': node.text.strip(), **node.attrib})
+    return result
+
+
+def load_structure(path):
+    with open(os.path.join(path, '_config/structure.xml')) as fp:
+        return xml_to_json(ElementTree.parse(fp).getroot())
+
